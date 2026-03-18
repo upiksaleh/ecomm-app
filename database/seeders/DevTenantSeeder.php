f59c8caee99a5faf2e5b90fd410cc91ca9887c1d
upiksaleh <upxsal@gmail.com>
2026-03-18 21:36:55 +0800

    update dev tenant seeder

--- database/seeders/DevTenantSeeder.php
+++ database/seeders/DevTenantSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Tenant;
+use App\Models\Tenant\Customer;
 use App\Models\Tenant\Product;
 use App\Models\Tenant\User;
 use Illuminate\Database\Seeder;
@@ -16,25 +17,22 @@ class DevTenantSeeder extends Seeder
     public function run(): void
     {
         $dev_id = 'dev';
-        $dev_tenant = Tenant::create(['id' => $dev_id]);
-        $dev_tenant->domains()->create(['domain' => $dev_id.'.localhost']);
+        $dev_tenant = Tenant::where('id', $dev_id)->first();
+        if (! $dev_tenant) {
+            $dev_tenant = Tenant::create(['id' => $dev_id]);
+            $dev_tenant->domains()->create(['domain' => $dev_id.'.'.env('APP_DOMAIN', 'localhost')]);
+        }
 
         tenancy()->initialize($dev_tenant);
+
         User::create([
             'name' => 'admin-'.$dev_id,
             'email' => 'admin-'.$dev_id.'@ecom.app',
             'password' => 'admin',
         ]);
 
-        for ($i = 1; $i <= 100; $i++) {
-            Product::create([
-                'name' => 'Sample Product '.$i,
-                'description' => 'This is a sample product '.$i,
-                'price' => $i.'99',
-                'sku' => 'SSP-00'.$i,
-                'quantity' => 100,
-                'active' => true,
-            ]);
-        }
+        Product::factory(100)->create();
+
+        Customer::factory(100)->create();
     }
 }
